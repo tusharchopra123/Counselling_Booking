c3059de2bb480b988750d88dd2b797e25bd51e21
tusharchopra123 <tusharchopra123@gmail.com>
2020-05-16 18:39:27 +0530

    server file changed

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path')
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+const server_port = process.env.PORT || 3371
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/api', require('./routes/api').route)
 
-app.listen(3371, () => console.log('Server started at http://localhost:3371'))
+app.listen(server_port)
